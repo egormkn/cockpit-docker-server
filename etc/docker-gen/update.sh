4f404184ce7c06ed74e27121f525c10af70d28e2
Egor Makarenko <egormkn@yandex.ru>
2019-07-04 01:33:07 +0300

    Update nginx config path for docker-gen

--- etc/docker-gen/update.sh
+++ etc/docker-gen/update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 systemctl stop nginx
-grep -e "# HOST: .*$" test.txt | sed "s/# HOST: //" | xargs -L1 certbot certonly --standalone -q -d
+grep -e "# HOST: .*$" /etc/nginx/sites-available/docker | sed "s/# HOST: //" | xargs -L1 certbot certonly --standalone -q -d
 systemctl start nginx
