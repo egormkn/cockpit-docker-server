057636228f81e3962c558d75235269438e09609a
Egor Makarenko <egormkn@yandex.ru>
2019-09-19 22:51:13 +0300

    Fix issue with certificates

--- etc/docker-gen/notify.sh
+++ etc/docker-gen/notify.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-systemctl stop nginx
-grep -e "# HOST: .*$" /etc/nginx/conf.d/docker.conf | sed "s/# HOST: //" | xargs -L1 certbot certonly --standalone -q -d
-systemctl start nginx
+grep -e "# HOST: .*$" /etc/nginx/conf.d/docker.conf | sed "s/# HOST: //" | xargs -L1 certbot run --nginx --redirect -q -d
+nginx -s reload
